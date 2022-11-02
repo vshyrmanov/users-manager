8f7b2a5ed182816412e97a12ca4de5d7d87a8e17
vshyrmanov <shyrmanovvm@gmail.com>
2022-11-02 23:34:26 +0200

    added main logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import AddUsers from "./components/Users/AddUsers";
+import UsersList from "./components/Users/UsersList";
+import {useState} from "react";
+
 function App() {
+   const [userList, setUsersList] = useState([]);
+
+    const addUserHandler = (userName, userAge) => {
+        setUsersList((prevUsersList) => {
+            return [
+                ...prevUsersList,
+                {name: userName, age: userAge, id: Math.random().toString()}
+            ]
+        });
+    }
+
   return (
     <div>
-      <h1>Add user</h1>
+      <AddUsers onAddUser={addUserHandler} />
+      <UsersList users={userList}/>
     </div>
   );
 }
